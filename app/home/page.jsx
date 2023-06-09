a631d6f1a63a9f084c7f061de1024c34cda20273
denis <denisalalykin2001@gmail.com>
2023-06-09 20:18:21 +0700

    FixBag

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -24,31 +24,32 @@ export default function Home() {
     const [todoInput, setTodoInput] = useState("");
     const [todos, setTodos] = useState([]);
     const router = useRouter();
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState("default");
+
     const [loading, setLoading] = useState(true);
     const db = getFirestore(app);
-    const getTodos = async () => {
-        console.log("entered");
-        if (!user) {
-            return; // user is null or undefined, so exit early
-        }
-        const q = query(collection(db, "todos"), where("owner", "==", user.uid));
-        let data = [];
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
-            data.push({ ...doc.data(), id: doc.id });
-        });
-        setTodos(data);
-        setLoading(false);
-    };
+    // const getTodos = async () => {
+    //     console.log("entered");
+    //     if (!user) {
+    //         return; // user is null or undefined, so exit early
+    //     }
+    //     const q = query(collection(db, "todos"), where("owner", "==", user.uid));
+    //     let data = [];
+    //     const querySnapshot = await getDocs(q);
+    //     querySnapshot.forEach((doc) => {
+    //         console.log(doc.id, " => ", doc.data());
+    //         data.push({ ...doc.data(), id: doc.id });
+    //     });
+    //     setTodos(data);
+    //     setLoading(false);
+    // };
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
                 // User is signed in, so set the user state variable
                 setUser(user);
                 setLoading(false);
-                getTodos();
+               // getTodos();
             } else {
                 // No user is signed in, so clear the user state variable
                 router.push("/login");
@@ -57,7 +58,7 @@ export default function Home() {
         });
 
         if (user) {
-            getTodos();
+           // getTodos();
         }
 
         return () => unsubscribe(); // unsubscribe from the listener when the component unmounts
